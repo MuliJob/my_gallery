d988b1b1ca7e25c171e20ecf3c0e37d0b8abb00f
MuliJob <munyoki912@gmail.com>
2024-10-08 13:02:28 +0300

    added homepage for the sight

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.home_page, name='home_page'),
+  path('user-dashboard/', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('contact/', views.contact, name='contact'),
   path('photo-details/', views.photo_detail, name='photo_detail'),
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+  return render(request, 'home/index.html')
+
 def home(request):
   return render(request, 'index.html')
 
