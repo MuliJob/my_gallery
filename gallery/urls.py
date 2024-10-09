4344c93e3f244181c732da01b7a083c71358d026
MuliJob <munyoki912@gmail.com>
2024-10-09 17:36:19 +0300

    added login and register path

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home_page, name='home_page'),
+  path('auth/register/', views.register, name='register'),
+  path('auth/login/', views.login, name='login'),
   path('user-dashboard/', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('contact/', views.contact, name='contact'),
