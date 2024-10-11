939871faee86331de04eddc4251cf68a9103da02
MuliJob <munyoki912@gmail.com>
2024-10-11 12:58:43 +0300

    added location and category models with save, update and delete methods

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def save_location(self):
+        self.save()
+
+    def update_location(self, name=None, description=None):
+        if name:
+            self.name = name
+        if description:
+            self.description = description
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def save_category(self):
+        self.save()
+
+    def update_category(self, name=None, description=None):
+        if name:
+            self.name = name
+        if description:
+            self.description = description
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
