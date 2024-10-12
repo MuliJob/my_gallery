6709f4cca857e07f999e4b89c75961e764c32326
MuliJob <munyoki912@gmail.com>
2024-10-12 15:46:33 +0300

    added video location and category models

--- gallery/models.py
+++ gallery/models.py
@@ -81,3 +81,44 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image_name
+    
+
+class VideoLocation(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def save_video_location(self):
+        self.save()
+
+    def update_video_location(self, name=None, description=None):
+        if name:
+            self.name = name
+        if description:
+            self.description = description
+        self.save()
+
+    def delete_video_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+    
+class VideoCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def save_video_category(self):
+        self.save()
+
+    def update_video_category(self, name=None, description=None):
+        if name:
+            self.name = name
+        if description:
+            self.description = description
+        self.save()
+
+    def delete_video_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
