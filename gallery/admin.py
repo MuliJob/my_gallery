11ff4ded9ef329986c67d4f300fcb38fb1b82da9
MuliJob <munyoki912@gmail.com>
2024-10-13 16:33:53 +0300

    added video models to the admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Location, Category, Image
+from .models import Location, Category, Image, Video, VideoCategory, VideoLocation
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Category)
 admin.site.register(Image)
+admin.site.register(Video)
+admin.site.register(VideoCategory)
+admin.site.register(VideoLocation)
